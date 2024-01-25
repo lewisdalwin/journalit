0078ebce259c2caca4b23726ea4a4e522f933684
Dalwin D. Lewis <lewisdalwin@gmail.com>
2024-01-25 14:26:13 -0600

    Initial commit of journalit

---
+++ cmd/web/main.go
@@ -0,0 +1,10 @@
+// Filename: cmd/web/main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to journalit!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lewisdalwin/journalit
+
+go 1.21.5
